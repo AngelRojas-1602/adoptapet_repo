2c9d361510b74f638fc0050240fe93621a49c684
Angel Nestor <angelnestor@MacBook-Pro-de-Angel.local>
2020-11-12 15:33:42 -0600

    Cambios de ajustes

--- config/env.sh
+++ config/env.sh
@@ -1,4 +1,6 @@
+echo "CONFIGURANDO VARIABLES"
 export NODE_ENV='development'
 export PORT=3000
 export SECRET='secret' # para mayor seguridad puedes cambiar esto por el secreto de tu preferencia
 export MONGODB_URI='mongodb+srv://introabd:cangetin123@cluster0.zyhnx.mongodb.net/AdoptaPet'
+echo "LISTAS VARIABLES"
